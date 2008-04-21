5e4c736407d1a3ed37994211879d75e9d365eeb2
dbloete <mail@dennisbloete.de>
2008-04-21 23:33:37 +0200

    oops, missed to add the migration

---
+++ db/migrate/002_add_type_identifier_to_release_policies.rb
@@ -0,0 +1,14 @@
+class AddTypeIdentifierToReleasePolicies < ActiveRecord::Migration
+  def self.up
+    add_column :release_policies, :type_identifier, :string
+    ReleasePolicy.find(:all).each do |release_policy|
+      release_policy.update_attribute(:type_identifier, release_policy.property)
+    end
+    remove_index :release_policies, :column => [:site_id, :property]
+    add_index :release_policies, [:site_id, :property, :type_identifier], :unique => true, :name => :unique_property
+  end
+
+  def self.down
+    remove_column :release_policies, :type_identifier
+  end
+end
