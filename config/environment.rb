2d80d34f5425a2376955130402793ca173b076de
dbloete <mail@dennisbloete.de>
2008-12-07 22:11:15 +0100

    Updated and added config files

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,11 @@ Rails::Initializer.run do |config|
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = APP_CONFIG['session']
   
+  # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
+  # All files from config/locales/*.rb,yml are added automatically.
+  # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
+  # config.i18n.default_locale = :de
+  
   # Mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
