dbbfb81da2c25d3f511401679e9650407b5e55af
dbloete <mail@dennisbloete.de>
2010-02-26 10:15:30 +0100

    Fixed some deprec warnungs and renewed exception_notification config

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,14 +1,4 @@
-ExceptionNotifier.exception_recipients = [Masquerade::Application::Config['email']]
-ExceptionNotifier.sender_address = %("#{Masquerade::Application::Config['name']}" <#{Masquerade::Application::Config['mailer']['from']}>)
-ExceptionNotifier.email_prefix = "[#{Masquerade::Application::Config['name']} Error] "
-
-# Email notifications will only occur when the IP address is determined not to
-# be local. You can specify certain addresses to always be local so that you'll
-# get a detailed error instead of the generic error page. You do this in your
-# controller (or even per-controller):
-# consider_local "64.72.18.143", "14.17.21.25"
-
-# The address "127.0.0.1" is always considered local. If you want to completely
-# reset the list of all addresses (for instance, if you wanted "127.0.0.1" to
-# NOT be considered local), you can simply do, somewhere in your controller:
-# local_addresses.clear
+Masquerade::Application.config.middleware.use ExceptionNotifier,
+  :email_prefix => "[#{Masquerade::Application::Config['name']} Error] ",
+  :sender_address => %("#{Masquerade::Application::Config['name']}" <#{Masquerade::Application::Config['mailer']['from']}>),
+  :exception_recipients => [Masquerade::Application::Config['email']]
