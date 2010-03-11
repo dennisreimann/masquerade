fefbf172f2ff876fe28e593462c2170473426766
dbloete <mail@dennisbloete.de>
2010-03-11 10:20:18 +0100

    Added missing translation and note for unsopported attributes

--- app/helpers/server_helper.rb
+++ app/helpers/server_helper.rb
@@ -2,14 +2,14 @@ module ServerHelper
   
   def sreg_request_for_field(field_name)
     if sreg_request.required.include?(field_name)
-      "required"
+      t(:required)
     elsif sreg_request.optional.include?(field_name)
-      "optional"
+      t(:optional)
     end
   end
   
   def ax_request_for_field(ax_attribute)
-    ax_attribute.required ? "required" : "optional"
+    ax_attribute.required ? t(:required) : t(:optional)
   end
   
 end
