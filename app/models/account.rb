cdd91392bb4a3db6b7339eecdc17ddc7cc9e001e
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2011-05-03 16:08:16 +0200

    blacklist route prefixes from possible account names

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,6 +15,8 @@ class Account < ActiveRecord::Base
   validates_presence_of :password_confirmation, :if => :password_required?
   validates_length_of :password, :within => 6..40, :if => :password_required?
   validates_confirmation_of :password, :if => :password_required?
+  # check `rake routes' for whether this list is still complete when routes are changed
+  validates_exclusion_of :login, :in => %w[account session password help safe-login forgot_password reset_password login logout server consumer]
   
   before_save   :encrypt_password
   before_create :make_activation_code
