fe154d7ffa8258faf9e0b54d443f4c536aa79bfc
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2011-05-03 16:53:21 +0200

    add button for resending activation email

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Masquerade::Application.routes.draw do
 
   get "/login" => "sessions#new", :as => :login
   get "/logout" => "sessions#destroy", :as => :logout
+  post '/resend_activation_email/*account' => 'accounts#resend_activation_email', :as => :resend_activation_email
 
   match "/server" => "server#index", :as => :server
   match "/server/decide" => "server#decide", :as => :decide
