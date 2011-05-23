cd57e4e034e06cdfb55d403c4f2d76adb93609c8
Florian Eitel <feitel@indeedgeek.de>
2011-05-23 18:40:33 +0200

    Move action_mailer.delivery_method into environments
    If delivery_method are set in initializers tests use :smtp too and many
    test are broken.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ Masquerade::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
   # Enable threaded mode
   # config.threadsafe!
 
